fbc10b7eca66f9baefc3cc619022bc2ad17fb5a2
xFreshu <lprzybyszcontact@gmail.com>
2022-05-15 18:11:39 +0200

    create basic nextjs template

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import { GlobalStyle } from '../styles/GlobalStyle';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
+};
 
-export default MyApp
+export default MyApp;
